861a5c36739915040517a3e38dbff45647b4acba
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-15 21:06:46 +0100

    'update_project'

--- meetup/views.py
+++ meetup/views.py
@@ -16,10 +16,12 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LogoutView
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
     
 # Login using FBV
+@csrf_protect
 def Login(request):
     if request.user.is_authenticated:
         return redirect('meetup-index')
@@ -39,6 +41,7 @@ def Login(request):
     return render(request, 'meetup/login.html')
   
 # Register
+@csrf_protect
 def Register(request):
     
     form = MyUserRegistrationForm()
