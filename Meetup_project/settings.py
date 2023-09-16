c3ac5af22bd72a481a0c1f622fd73367fa381d7e
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-16 18:39:18 +0100

    'update_project'

--- Meetup_project/settings.py
+++ Meetup_project/settings.py
@@ -38,9 +38,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-a1d4.up.railway.app/','https://*.127.0.0.1:8000' ]
+ALLOWED_HOSTS = ['*' ]
 
 # csrf trusted
 CSRF_TRUSTED_ORIGINS = ['https://*.https://web-production-a1d4.up.railway.app','https://*.127.0.0.1:8000']
