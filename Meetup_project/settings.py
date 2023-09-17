1cfb67c5b2045f589d9010a3bce0e1dbe875561c
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-17 10:59:31 +0100

    'Updated'

--- Meetup_project/settings.py
+++ Meetup_project/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -101,23 +101,23 @@ WSGI_APPLICATION = 'Meetup_project.wsgi.application'
 
 AUTH_USER_MODEL = 'meetup.myUser' #maunly create databasemodel
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DATABASE_NAME'), 
-#         'USER': config('DATABASE_USER'), 
-#         'PASSWORD': config('DATABASE_PASS'),
-#         'HOST': config('DATABASE_HOST'), 
-#         'PORT': config('DATABASE_PORT'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+        
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'), 
+        'USER': config('DATABASE_USER'), 
+        'PASSWORD': config('DATABASE_PASS'),
+        'HOST': config('DATABASE_HOST'), 
+        'PORT': config('DATABASE_PORT'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
