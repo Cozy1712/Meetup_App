4f327edfe128acc3ea0b997f6a42af26449618d7
cozy1712 <kolawolekabiru89@gmail.com>
2023-09-24 16:35:15 +0100

    'updated'

--- Meetup_project/urls.py
+++ Meetup_project/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cozy1712/', admin.site.urls),
     path('', include('meetup.urls')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
